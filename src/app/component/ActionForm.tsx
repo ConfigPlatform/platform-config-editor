8f1cdb69e54cc7ad77fa8c7bcfca062c75c7dd47
Kateryna Tymofieieva <katatimofeeva82@gmail.com>
2024-02-09 09:39:07 -0800

    add styles

--- src/app/component/ActionForm.tsx
+++ src/app/component/ActionForm.tsx
@@ -10,14 +10,15 @@ const ActionForm = () => {
     e.preventDefault();
     console.log(inputValue); // Handle the submit event and the input value
     // Implement what should happen on submit, e.g., send data to an API
+    setInputValue('');
   };
 
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center w-1/2">
       <form onSubmit={handleSubmit} className="w-full max-w-xl p-8 bg-white rounded-lg shadow-md">
-        <div className="mb-4">
+        <div>
           <textarea
-            className="form-textarea mt-1 block w-full border-0 border-b-2 border-gray-300 focus:border-blue-500 focus:ring-0"
+            className="form-textarea mt-1 block w-full border rounded-lg border-gray-300   focus:border-gray-500 focus:ring-0"
             id="message"
             rows={4}
             placeholder="Your message here..."
@@ -25,7 +26,7 @@ const ActionForm = () => {
             onChange={(e) => setInputValue(e.target.value)}
           ></textarea>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between px-3 py-4">
           <Button type={'submit'}>Send</Button>
         </div>
       </form>
