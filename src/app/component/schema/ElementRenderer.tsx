0070ad19e5228282d683576119a2a3da07c9d574
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2024-03-03 15:04:26 -0800

    Button create

--- src/app/component/schema/ElementRenderer.tsx
+++ src/app/component/schema/ElementRenderer.tsx
@@ -1,16 +1,20 @@
+import React from "react";
 import {get} from "lodash";
 import ElementListRenderer from "@/app/component/schema/ElementListRenderer";
 import Text from './Text';
-import Button from './button';
+import Button from './Button';
 
-const componentMap: {[name: string]: any} = {
+const componentMap: { [name: string]: React.ComponentType<any> } = {
     text: Text,
     button: Button,
 }
 
 interface IProps {
     ml: number;
-    element: object;
+    element: {
+        type: string;
+        content: any[];
+    };
 }
 
 const ElementRenderer = ({element, ml}: IProps) => {
@@ -18,10 +22,27 @@ const ElementRenderer = ({element, ml}: IProps) => {
     const content = get(element, 'content', []);
     const Component = componentMap[type];
 
+    // Check if Component exists and its type is 'button'
+    const isButton = type === 'button';
+
     return <>
-        {Component && <div style={{ marginLeft: `${ml}px`, marginTop: '10px' }}><Component {...element} /></div>}
-        <ElementListRenderer ml={ml+20} content={content} />
+        {Component && !isButton && (
+            <>
+                <div style={{ marginLeft: `${ml}px`, marginTop: '10px' }}>
+                    <Component {...element} />
+                </div>
+                <ElementListRenderer ml={ml + 20} content={content}/>
+            </>
+        )}
+
+        {Component && isButton && (
+            <div style={{ marginLeft: `${ml}px`, marginTop: '10px' }}>
+                <Component {...element} >
+                    <ElementListRenderer ml={ml} content={content}/>
+                </Component>
+            </div>
+        )}
     </>
 }
 
-export default ElementRenderer
+export default ElementRenderer;
