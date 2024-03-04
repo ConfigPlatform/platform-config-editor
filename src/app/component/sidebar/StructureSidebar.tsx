455d183f87d7b2baf0990a5af02203a0dd0df6e8
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2024-03-04 13:02:48 -0800

    Entities structure

--- src/app/component/sidebar/StructureSidebar.tsx
+++ src/app/component/sidebar/StructureSidebar.tsx
@@ -2,17 +2,21 @@
 
 import useConfigurationStore from "@/app/store/configurationStore";
 import {get} from "lodash";
-import PageStructure from "@/app/component/structure/PageStructure";
+import PageStructure, {Page} from "@/app/component/structure/PageStructure";
+import EntityStructure, {Entity} from "@/app/component/structure/EntityStructure";
 
 const StructureSidebar = () => {
     const pages = useConfigurationStore((state) => get(state, 'configuration.pages', []));
+    const entities = useConfigurationStore((state) => get(state, 'configuration.entities', []));
 
     return (
         <aside className={'h-screen w-1/12 border-gray-200 bg-gray-100'}>
-        <b>Pages:</b>
-        <PageStructure pages={pages} />
+            <h3 className='font-bold'>Pages:</h3>
+            <PageStructure pages={pages as Page[]} />
+            <h3 className='font-bold mt-5'>Entities:</h3>
+            <EntityStructure entities={entities as Entity[]} />
         </aside>
-);
+    );
 }
 
 export default StructureSidebar
--- src/app/component/structure/ElementRenderer.tsx
+++ src/app/component/structure/ElementRenderer.tsx
@@ -7,18 +7,28 @@ import useConfigurationStore from "@/app/store/configurationStore";
 interface IProps {
     ml: number;
     element: object;
+    title?: string;
 }
 
-const ElementRenderer = ({element, ml}: IProps) => {
-    const { selectElement } = useConfigurationStore.getState();
+const ElementRenderer = ({element, ml, title}: IProps) => {
+    const {selectElement} = useConfigurationStore.getState();
 
     const type = get(element, 'type', '');
+    const name = get(element, 'name', '');
     const content = get(element, 'content', []);
+    const renderEl = (title && title === 'entities') ? name : type;
 
-    return <div>
-        <p className={'hover:text-blue-700 cursor-pointer'} style={{ marginLeft: `${ml}px` }} onClick={() => selectElement(element)}>{type}</p>
-        <ElementListRenderer ml={ml} content={content} />
-    </div>
+    return (
+        <div>
+            <p
+                className={'hover:text-blue-700 cursor-pointer'}
+                style={{marginLeft: `${ml}px`}}
+                onClick={() => selectElement(element)}>
+                {renderEl}
+            </p>
+            <ElementListRenderer ml={ml} content={content}/>
+        </div>
+    );
 }
 
 export default ElementRenderer
