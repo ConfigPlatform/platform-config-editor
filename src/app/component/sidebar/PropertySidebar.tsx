e72110a64afc6c48c5de3b83bf61b0ca0d635fb7
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2024-03-04 15:09:14 -0800

    Remove sidebar and structure directories

--- src/app/component/sidebar/PropertySidebar.tsx
+++
@@ -1,9 +0,0 @@
-const StructureSidebar = () => {
-    return (
-        <aside className={'h-screen w-3/12 border-gray-200 bg-gray-100'}>
-            properties
-        </aside>
-    );
-}
-
-export default StructureSidebar
