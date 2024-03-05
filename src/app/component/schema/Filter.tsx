32fe932ac048207fca79188589fe63a64c59f574
e-nie <99044956+e-nie@users.noreply.github.com>
2024-03-05 20:44:01 +0100

    fix Filter.tsx

--- src/app/component/schema/Filter.tsx
+++ src/app/component/schema/Filter.tsx
@@ -4,7 +4,7 @@ const Filter = (props: object) => {
     const filterBy = get(props, 'filterBy', '');
 
     return <div style={{ width: '100px', height: '50px' }} className="border-dashed border-2 border-sky-500 rounded-md flex items-center justify-center">
-        <p>Text - {filterBy}</p>
+        <p>Filter - {filterBy}</p>
 
     </div>
 }
