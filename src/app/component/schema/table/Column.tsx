4260a07cb50a11642c5f57ea9ee92b20da2b895f
Akradabas <akradabaska@gmail.com>
2024-03-06 22:24:01 -0800

    complete_table_template

--- src/app/component/schema/table/Column.tsx
+++ src/app/component/schema/table/Column.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 import {FC} from 'react';
 
 interface IProps {
-    label : string,
     name: string,
-    type: string,
-    dataType: any
+    type?: string,
+    dataType?: any
 }
 
-const ColumnComponent: FC<IProps> = ({ label, name, type, dataType }) => {
+const ColumnComponent: FC<IProps> = ({name, type, dataType }) => {
     return (
-        <div className="mb-4">
-            <label htmlFor={name} className="block text-sm font-medium text-gray-700 mr-2">{label}</label>
+        <div>
+            {name}
         </div>
     );
 };
--- src/app/component/schema/table/Table.tsx
+++ src/app/component/schema/table/Table.tsx
@@ -1,65 +1,43 @@
 import React from 'react';
 import Column from './Column';
-import {get} from 'lodash';
+import { get } from 'lodash';
+
+interface Icolumns {
+  type: 'container',
+  className: string,
+  name: string,
+  sortable: boolean,
+  cell: {
+    className: string,
+    content: {
+      type: string,
+      value: string,
+      vars: {
+        id: string
+      }
+    }[]
+  }
+}
 
 const FormLayoutComponent = (props: any) => {
+  const columns: Icolumns[] = get(props, 'columns', []);
 
-  console.log(props.columns, '333333333')
-  
-  const tableId = get(props, 'id', '')
-  const columns = get(props, 'columns', [])
-  // columns.map(el => {
-  //   if(el.cell.content[0].value) {
-  //     console.log(el.cell.content[0].value, '111111')
-  //   } else if (el.cell.content[0].content) {
-  //     console.log(el.cell.content[0].content, '222222')
-  //   } else {
-  //     console.log('else')
-  //   }
-  // })
-
-  function mapper(columns) {
-    columns.map(el => {
-      console.log(el, 'oleg')
-      if(el.cell?.content[0].value) {
-        console.log(el.cell.content[0].value)
-      } else if (el.cell?.content[0].content){
-        mapper(el.cell.content[0].content)
-      } else if(el.value) {
-        console.log(el.value, 'VALUE!!!!!')
-      }
-       else {
-        console.log('Error')
-      }
-    })
-  }
-
-  mapper(columns);
-  
   return (
-    <div
-      className="border-dashed border-2 border-sky-500 rounded-md flex flex-col p-4"
-      style={{width: '400px', height: '200px'}}
-    >
+    <div className="border-dashed border-2 border-sky-500 rounded-md flex flex-col p-2 w-10/12">
       {/* "Table" в левом верхнем углу */}
       <div className="self-start mb-4">
         <p>Table</p>
       </div>
 
       {/* Контейнер для колонок */}
-      <table className="relative flex items-center">
-
-
-        {/* Колонка "Data" */}
-        <div className="flex items-center">
-          <div
-            style={{width: '100px', height: '50px'}}
-            className="border-dashed border-2 border-sky-500 rounded-md flex items-center justify-center mr-2"
-          >
-            <Column label="Date" name="date" type="text" dataType={null} />
+      <table className="relative flex items-center w-full justify-evenly">
+        {columns.map((el) => (
+          <div key={el.name} className="flex items-center">
+            <div className="border-dashed border-2 border-sky-500 rounded-md flex items-center justify-center p-2 min-w-24">
+              <Column name={el.name} />
+            </div>
           </div>
-        </div>
-        
+        ))}
       </table>
     </div>
   );
