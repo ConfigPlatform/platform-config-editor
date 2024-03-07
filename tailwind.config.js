07060bff46a2ae3e5d4379ef89dd910134ba0afc
Andrew1326 <maxgreen007007@gmail.com>
2024-03-07 21:13:21 +0000

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // purge: [], // Purge отключен
-  content: ['./src/**/*.{ts,tsx}'],
-
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-
-  // safelist: [
-  //     {
-  //         // плохая практика, но классы динамические и надо тут прописать то что мы будем использовать
-  //         pattern: /./, // Регулярное выражение для соответствия любому классу
-  //     },
-  // ],
 };
