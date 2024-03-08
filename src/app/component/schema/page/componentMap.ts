a91bba349cd3c170581a4dc33f6008175e473d65
e-nie <99044956+e-nie@users.noreply.github.com>
2024-03-08 23:31:47 +0100

    add Pagination element

--- src/app/component/schema/page/componentMap.ts
+++ src/app/component/schema/page/componentMap.ts
@@ -5,6 +5,8 @@ import Row from '@/app/component/schema/page/Row';
 import Col from '@/app/component/schema/page/Col';
 import Table from '@/app/component/schema/table/Table';
 import Filter from '@/app/component/schema/page/Filter';
+import pagination from '@/app/component/schema/page/Pagination';
+import Pagination from '@/app/component/schema/page/Pagination';
 
 const componentMap: {[name: string]: any} = {
   text: Text,
@@ -14,6 +16,7 @@ const componentMap: {[name: string]: any} = {
   col: Col,
   table: Table,
   filter: Filter,
+  pagination: Pagination,
 };
 
 export default componentMap;
