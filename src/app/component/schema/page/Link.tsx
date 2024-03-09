95506e6d714534a2a1655e5c55b871cf4b3003b7
StanislavZemskov <zsv630@gmail.com>
2024-03-08 23:48:50 -0500

    Link.tsx

---
+++ src/app/component/schema/page/Link.tsx
@@ -0,0 +1,19 @@
+import {get} from 'lodash';
+import React from 'react';
+import BlockLabel from '@/app/component/schema/page/BlockLabel';
+
+const Link = (props: object) => {
+  const text = get(props, 'text', '');
+  const href = get(props, 'href', '');
+  console.log(props, 'props');
+
+  return (
+    <div style={{minWidth: '100px'}} className="m-2 p-1 border-dashed border border-red-600">
+      <BlockLabel label={'Link'} />
+      <p>{text}</p>
+      <p>{href}</p>
+    </div>
+  );
+};
+
+export default Link;
--- src/app/component/schema/page/componentMap.ts
+++ src/app/component/schema/page/componentMap.ts
@@ -5,9 +5,11 @@ import Row from '@/app/component/schema/page/Row';
 import Col from '@/app/component/schema/page/Col';
 import Table from '@/app/component/schema/table/Table';
 import Filter from '@/app/component/schema/page/Filter';
+import Link from '@/app/component/schema/page/Link';
 
 const componentMap: {[name: string]: any} = {
   text: Text,
+  link: Link,
   button: Button,
   container: Container,
   row: Row,
