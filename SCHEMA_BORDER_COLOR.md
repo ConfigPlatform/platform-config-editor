9c99fb0484724b6f0e1da51bf82d7ab6e668ba1b
e-nie <99044956+e-nie@users.noreply.github.com>
2024-03-09 20:02:26 +0100

    fix border color for Pagination

--- SCHEMA_BORDER_COLOR.md
+++ SCHEMA_BORDER_COLOR.md
@@ -7,3 +7,4 @@ Col - indigo-500
 Button - lime-500
 Text - violet-500
 Filter - yellow-500
+Pagination - fuchsia-500
--- src/app/component/schema/page/Pagination.tsx
+++ src/app/component/schema/page/Pagination.tsx
@@ -3,7 +3,7 @@ import BlockLabel from '@/app/component/schema/page/BlockLabel';
 
 const Pagination = (props: object) => {
   return (
-    <div className="border-dashed border border-yellow-500 p-1 m-2">
+    <div className="border-dashed border border-fuchsia-500 p-1 m-2">
       <BlockLabel label={'Pagination'} />
     </div>
   );
