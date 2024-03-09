079ec50a14ed16b1d16b1e764af3428664b08de0
StanislavZemskov <zsv630@gmail.com>
2024-03-08 23:58:50 -0500

    console

--- src/app/component/schema/page/Link.tsx
+++ src/app/component/schema/page/Link.tsx
@@ -5,7 +5,6 @@ import BlockLabel from '@/app/component/schema/page/BlockLabel';
 const Link = (props: object) => {
   const text = get(props, 'text', '');
   const href = get(props, 'href', '');
-  console.log(props, 'props');
 
   return (
     <div style={{minWidth: '100px'}} className="m-2 p-1 border-dashed border border-red-600">
