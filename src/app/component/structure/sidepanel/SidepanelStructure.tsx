5a15a25509e96bdc0d5ef1708e33b7f0b2d2d750
Akradabas <akradabaska@gmail.com>
2024-03-11 14:16:19 -0700

    updated_sipanel_template

---
+++ src/app/component/structure/sidepanel/SidepanelStructure.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import useConfigurationStore from '@/app/store/configurationStore';
+import {nanoid} from 'nanoid';
+
+
+export interface ISidepanel {
+  className: string;
+  content: IContent[];
+}
+
+export interface IContent {
+  content: IContentContent[];
+  path: string;
+  type: string;
+}
+export interface IContentContent {
+  className: string;
+  type: string;
+  value: string;
+}
+
+interface IProps {
+  sidepanel: object;
+}
+
+const SidepanelStructure = ({sidepanel}: IProps) => {
+  return (
+    <div>
+      <p className={'text-xs font-bold'}>Sidepanel</p>
+
+      {sidepanel.map((el, i) => (
+        <div key={nanoid()} style={{marginLeft: '4px'}}>
+          <b
+            className={'hover:text-blue-700 cursor-pointer text-xs'}
+            onClick={() => useConfigurationStore.setState({structurePath: `sidepanels[${i}]`})}
+          >
+            {el.path}
+          </b>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SidepanelStructure;
