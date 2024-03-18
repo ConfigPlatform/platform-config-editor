231d06649514204c4427fe0582508e120543d9e5
Irina Gayfulina <63535542+gayfulina@users.noreply.github.com>
2024-03-18 13:14:44 -0700

    create tooltip

--- src/app/component/schema/page/Tooltip.tsx
+++ src/app/component/schema/page/Tooltip.tsx
@@ -2,20 +2,24 @@ import {get} from 'lodash';
 import React from 'react';
 import BlockLabel from '@/app/component/schema/BlockLabel';
 import ComponentSelect from '@/app/component/wrapper/ComponentSelect';
+import ElementListRenderer from '@/app/component/schema/page/ElementListRenderer';
 
 interface ITooltip {
   value: string;
   path: string;
+  content: object[];
 }
 
 const Tooltip = (props: ITooltip) => {
   const value = get(props, 'value', '');
   const path = get(props, 'path', '');
+  const content = get(props, 'content', []);
 
   return (
     <ComponentSelect path={path} className={'m-2 p-1 border-dashed border border-amber-500'}>
       <BlockLabel label={'Tooltip'} />
       <p className={'text-sm'}>{value}</p>
+      <ElementListRenderer content={content} path={`${path}.content`} />
     </ComponentSelect>
   );
 };
